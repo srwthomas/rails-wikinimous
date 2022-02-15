5b60a8fc2efa0537ab5fdb6cc4c55ee79ca1f936
scott thomas <srwthomas1@gmail.com>
2022-02-15 16:06:20 +0100

    built structure & methods - routes not yet functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+  get 'articles/new', to: 'articles#new', as: :new
+
+  get 'article/:id/edit', to: 'articles#edit', as: :edit
+  get 'article/:id', to: 'articles#show', as: :article
+
+  delete 'article/:id', to: 'articles#destroy'
+  patch 'article/:id', to: 'articles#update'
+  post '/', to: 'articles#create'
 end
